8d12ea9d54ebf5e75295f98e0fa4df61b13bbd7d
Mike Hale <mike@stompgear.com>
2022-06-01 16:43:14 -0500

    initial commit

---
+++ pages/api/createTransaction.js
@@ -0,0 +1,113 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Transaction
+} from "@solana/web3.js";
+import {
+    createTransferCheckedInstruction,
+    getAssociatedTokenAddress,
+    getMint
+} from "@solana/spl-token"
+import BigNumber from "bignumber.js";
+import products from "./products.json";
+
+const usdcAddress = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
+const sellerAddress = 'B1aLAAe4vW8nSQCetXnYqJfRxzTjnbooczwkUJAr7yMS'
+const sellerPublicKey = new PublicKey(sellerAddress);
+
+const createTransaction = async (req, res) => {
+  try {
+    // Extract the transaction data from the request body
+    const { buyer, orderID, itemID } = req.body;
+
+    // If we don't have something we need, stop!
+    if (!buyer) {
+      return res.status(400).json({
+        message: "Missing buyer address",
+      });
+    }
+
+    if (!orderID) {
+      return res.status(400).json({
+        message: "Missing order ID",
+      });
+    }
+
+    // Fetch item price from products.json using itemID
+    const itemPrice = products.find((item) => item.id === itemID).price;
+
+    if (!itemPrice) {
+      return res.status(404).json({
+        message: "Item not found. please check item ID",
+      });
+    }
+
+    // Convert our price to the correct format
+    const bigAmount = BigNumber(itemPrice);
+    const buyerPublicKey = new PublicKey(buyer);
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = clusterApiUrl(network);
+    const connection = new Connection(endpoint);
+
+    const buyerUsdcAddress = await getAssociatedTokenAddress(usdcAddress, buyerPublicKey)
+    const shopUsdcAddress = await getAssociatedTokenAddress(usdcAddress, sellerPublicKey)
+
+    // A blockhash is sort of like an ID for a block. It lets you identify each block.
+    const { blockhash } = await connection.getLatestBlockhash("finalized");
+
+    const usdcMint = await getMint(connection, usdcAddress);
+
+    // The first two things we need - a recent block ID
+    // and the public key of the fee payer
+    const tx = new Transaction({
+      recentBlockhash: blockhash,
+      feePayer: buyerPublicKey,
+    });
+
+    // This is the "action" that the transaction will take
+    // We're just going to transfer some SOL
+    const transferInstruction = createTransferCheckedInstruction(
+        buyerUsdcAddress,
+        usdcAddress,
+        shopUsdcAddress,
+        buyerPublicKey,
+        bigAmount.toNumber() * 10 ** (await usdcMint).decimals,
+        usdcMint.decimals
+    );
+
+    // We're adding more instructions to the transaction
+    transferInstruction.keys.push({
+      // We'll use our OrderId to find this transaction later
+      pubkey: new PublicKey(orderID),
+      isSigner: false,
+      isWritable: false,
+    });
+
+    tx.add(transferInstruction);
+
+    // Formatting our transaction
+    const serializedTransaction = tx.serialize({
+      requireAllSignatures: false,
+    });
+    const base64 = serializedTransaction.toString("base64");
+
+    res.status(200).json({
+      transaction: base64,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: "error creating tx" });
+    return;
+  }
+}
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    createTransaction(req, res);
+  } else {
+    res.status(405).end();
+  }
+}
